6958f13c03bd2e48ba2777bbbebaebb85bfce8b8
Gyrotank <Dmytro_Glomozda@epam.com>
2018-12-11 23:08:32 +0200

    Commented out sending report email after test completion

--- src/main/java/com/w2a/listeners/CustomListeners.java
+++ src/main/java/com/w2a/listeners/CustomListeners.java
@@ -20,101 +20,104 @@ import com.w2a.utilities.MonitoringMail;
 import com.w2a.utilities.TestConfig;
 import com.w2a.utilities.Utilities;
 
-public class CustomListeners extends AbstractPage implements ITestListener,ISuiteListener {
+public class CustomListeners extends AbstractPage implements ITestListener, ISuiteListener {
 
-	public 	String messageBody;
-	public void onFinish(ITestContext arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+    public String messageBody;
 
-	public void onStart(ITestContext arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void onFinish(ITestContext arg0) {
+        // TODO Auto-generated method stub
 
-	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	public void onTestFailure(ITestResult arg0) {
+    public void onStart(ITestContext arg0) {
+        // TODO Auto-generated method stub
 
-		System.setProperty("org.uncommons.reportng.escape-output","false");
-		try {
-			Utilities.captureScreenshot();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		test.log(LogStatus.FAIL, arg0.getName().toUpperCase()+" Failed with exception : "+arg0.getThrowable());
-		test.log(LogStatus.FAIL, test.addScreenCapture(Utilities.screenshotName));
-		
-		
-		
-		Reporter.log("Click to see Screenshot");
-		Reporter.log("<a target=\"_blank\" href="+Utilities.screenshotName+">Screenshot</a>");
-		Reporter.log("<br>");
-		Reporter.log("<br>");
-		Reporter.log("<a target=\"_blank\" href="+Utilities.screenshotName+"><img src="+Utilities.screenshotName+" height=200 width=200></img></a>");
-		rep.endTest(test);
-		rep.flush();
-		
-	}
+    }
 
-	public void onTestSkipped(ITestResult arg0) {
+    public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onTestFailure(ITestResult arg0) {
+
+        System.setProperty("org.uncommons.reportng.escape-output", "false");
+        try {
+            Utilities.captureScreenshot();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        test.log(LogStatus.FAIL, arg0.getName().toUpperCase() + " Failed with exception : " + arg0.getThrowable());
+        test.log(LogStatus.FAIL, test.addScreenCapture(Utilities.screenshotName));
 
 
-		test.log(LogStatus.SKIP, arg0.getName().toUpperCase()+" Skipped the test as the Run mode is NO");
-		rep.endTest(test);
-		rep.flush();
-		
-	}
+        Reporter.log("Click to see Screenshot");
+        Reporter.log("<a target=\"_blank\" href=" + Utilities.screenshotName + ">Screenshot</a>");
+        Reporter.log("<br>");
+        Reporter.log("<br>");
+        Reporter.log("<a target=\"_blank\" href=" + Utilities.screenshotName + "><img src=" + Utilities.screenshotName + " height=200 width=200></img></a>");
+        rep.endTest(test);
+        rep.flush();
+
+    }
+
+    public void onTestSkipped(ITestResult arg0) {
 
 
-	public void onTestStart(ITestResult arg0) {
+        test.log(LogStatus.SKIP, arg0.getName().toUpperCase() + " Skipped the test as the Run mode is NO");
+        rep.endTest(test);
+        rep.flush();
 
-		test = rep.startTest(arg0.getName().toUpperCase());
-	
-	}
-
-	public void onTestSuccess(ITestResult arg0) {
+    }
 
 
-		test.log(LogStatus.PASS, arg0.getName().toUpperCase()+" PASS");
-		rep.endTest(test);
-		rep.flush();
-		
-	}
+    public void onTestStart(ITestResult arg0) {
 
-	public void onFinish(ISuite arg0) {
-		
-		MonitoringMail mail = new MonitoringMail();
-		 
-		try {
-			messageBody = "http://" + InetAddress.getLocalHost().getHostAddress()
-					+ ":8080/job/LiveProject%20-%20PageObjectModel/Extent_Report/";
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		try {
-			mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
-		} catch (AddressException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        test = rep.startTest(arg0.getName().toUpperCase());
 
-		
-	}
+    }
 
-	public void onStart(ISuite arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void onTestSuccess(ITestResult arg0) {
+
+
+        test.log(LogStatus.PASS, arg0.getName().toUpperCase() + " PASS");
+        rep.endTest(test);
+        rep.flush();
+
+    }
+
+    public void onFinish(ISuite arg0) {
+
+//        sendMail();
+
+    }
+
+    public void onStart(ISuite arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void sendMail() {
+        MonitoringMail mail = new MonitoringMail();
+
+        try {
+            messageBody = "http://" + InetAddress.getLocalHost().getHostAddress()
+                    + ":8080/job/LiveProject%20-%20PageObjectModel/Extent_Report/";
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+        } catch (AddressException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
